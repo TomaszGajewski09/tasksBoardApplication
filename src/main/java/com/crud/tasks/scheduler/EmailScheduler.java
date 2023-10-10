458e3fc4af0b031198c9804656bd753e7704f4d7
TomaszGajewski09 <tomasz.gajewski0922@gmail.com>
2023-10-10 23:31:29 +0200

    23.4 Scheduler

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -24,7 +24,7 @@ public class EmailScheduler {
                 new Mail(
                         adminConfig.getAdminMail(),
                         SUBJECT,
-                        "Currently in database you got: " + size + ((size == 1) ? "task." : "tasks.")
+                        "Currently in database you got: " + size + ((size == 1) ? " task." : " tasks.")
                 )
         );
     }
