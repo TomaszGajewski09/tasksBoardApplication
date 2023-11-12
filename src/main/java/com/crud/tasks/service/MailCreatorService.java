b85c8bf9cb26f3e3bfffdc78bc6b82d33e22ffb8
TomaszGajewski09 <126516615+TomaszGajewski09@users.noreply.github.com>
2023-11-12 18:05:46 +0000

    32.3 Conditional instructions in the template

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,22 +7,39 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
     @Autowired
     private AdminConfig adminConfig;
 
+
     @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
+      
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://tomaszgajewski09.github.io");
         context.setVariable("button", "Visit website");
-        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("preview_message", "The card was created");
+        context.setVariable("goodbye_message", "Best regards,");
+        context.setVariable("company_name", "Comapny");
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", true);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 
